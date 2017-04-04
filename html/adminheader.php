7e0449038139627cfb6137727c23f092af627947
Rajeevkumar Yadav <rajeev11430@gmail.com>
2017-04-04 19:08:28 +0530

    common to all pages

---
+++ html/adminheader.php
@@ -0,0 +1,41 @@
+
+<?php
+
+?>
+
+<!DOCTYPE html>
+<html lang="en-US">
+	<head>
+	<title><?=$title?> - Centralized Monitoring System</title>
+		<meta charset="utf-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<link rel="stylesheet" type="text/css" href="../css/adminstylesheet.css">
+		<link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">
+		<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.0/jquery.min.js"></script>
+  		<script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
+  		<script src="//netsh.pp.ua/upwork-demo/1/js/typeahead.js"></script>
+
+
+  		
+	</head>
+   
+   	<body>	
+    
+        <div class="container-fluid">
+
+        	<div class="row header_row">
+				<div class="col-md-2 o" >
+					<div id="logo">   
+						<a href="#"><img src="../imgs/monk.png" width="150px" height="150px"></a>
+					</div>
+				</div>
+				<div class="col-md-10 o heading">
+					Centralized Monitoring System
+				</div>
+			</div>
+
+		</div>	
+
+	
+
+
