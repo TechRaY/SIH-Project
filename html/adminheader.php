fd34cc1a459ca3e22362f52484207c0773948db8
mitensolanki <miten.solanki@ves.ac.in>
2017-04-22 00:35:54 +0530

    pointed out mistakes on the first page solved

--- html/adminheader.php
+++ html/adminheader.php
@@ -1,41 +1,42 @@
-
 <?php
-
+include('conn.php');
+session_start();
 ?>
 
-<!DOCTYPE html>
-<html lang="en-US">
-	<head>
-	<title><?=$title?> - Centralized Monitoring System</title>
-		<meta charset="utf-8">
-		<meta name="viewport" content="width=device-width, initial-scale=1">
-		<link rel="stylesheet" type="text/css" href="../css/adminstylesheet.css">
-		<link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">
-		<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.0/jquery.min.js"></script>
-  		<script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
-  		<script src="//netsh.pp.ua/upwork-demo/1/js/typeahead.js"></script>
+<html>
+<head>
+  <title>SIH</title>
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+</head>
+<body>
 
 
-  		
-	</head>
-   
-   	<body>	
-    
-        <div class="container-fluid">
+<nav class="navbar navbar-default">
+  <div class="container-fluid">
+    <div class="navbar-header">
+      <a class="navbar-brand" href="index.php">Skill Course finder</a>
+    </div>
+    <ul class="nav navbar-nav">
+      <li><a href="add.php">Add College</a></li>
+    </ul>
 
-        	<div class="row header_row">
-				<div class="col-md-2 o" >
-					<div id="logo">   
-						<a href="#"><img src="../imgs/monk.png" width="150px" height="150px"></a>
-					</div>
-				</div>
-				<div class="col-md-10 o heading">
-					Centralized Monitoring System
-				</div>
-			</div>
+   <form class="form-horizontal" action="" method="post">
+    <div class="form-group">
+      <div class="col-sm-3"> 
+         <input type="text" name="search" class="form-control"required>
+      </div>
 
-		</div>	
-
-	
-
-
+      <div class="col-sm-3">
+           <input type="submit" name="searchquery" class="btn btn-sm btn-primary" value="Search">
+      </div>
+     </div>
+   </form>
+      <a href="logout.php">LOG OUT</a>
+  </div>
+ </div> 
+</nav>
+</body>
+</head>
+</html>
